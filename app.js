f5fe6b3e0fb0ec621c363270b1e597813f9eca51
rschpdr <pedro.resch@gmail.com>
2020-10-05 20:38:33 -0300

    dont remember lol

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 // Configura o app para entender requisições com tipo de corpo JSON
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 const projectRouter = require("./routes/project.routes");
 
